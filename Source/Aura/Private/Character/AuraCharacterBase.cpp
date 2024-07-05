a64b54ed9cc911f6368c9c21c2ee3c0f0888128b
Henry Graves <henrygraves95@gmail.com>
2024-07-05 05:56:19 -0400

    player and enemy characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// (C) J St Studio 2024
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// (C) J St Studio 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
