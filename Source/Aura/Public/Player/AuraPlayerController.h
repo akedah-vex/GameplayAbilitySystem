e3c9405cd5531ef58f8ddce581c9104543ef1450
Henry Graves <henrygraves95@gmail.com>
2024-07-05 21:38:25 -0700

    add enhanced input actions

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// (C) J St Studio 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
