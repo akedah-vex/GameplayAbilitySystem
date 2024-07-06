761df9d1e2c166b8dc905b3199a92c58d31a1b38
Henry Graves <henrygraves95@gmail.com>
2024-07-05 22:45:38 -0700

    add custom game mode and contrtoller settttings

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// (C) J St Studio 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
