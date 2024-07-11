7e75edc3e92982b1e7859c72a8589a4aece16789
Henry Graves <henrygraves95@gmail.com>
2024-07-11 08:38:10 -0700

    add MVC pattern for UI control

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// (C) J St Studio 2024
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
